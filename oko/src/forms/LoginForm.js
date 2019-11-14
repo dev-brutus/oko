f5adc4c700b1e2141e0236fafdb7ddcc668e2796
dev-brutus <dev-brutus@users.noreply.github.com>
2019-11-14 10:52:38 +0300

    Login button

--- oko/src/forms/LoginForm.js
+++ oko/src/forms/LoginForm.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function LoginForm() {
-    return <div>Login form</div>
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    onClick() {
+        console.log('Click');
+    }
+
+    render() {
+        return <div><a className="waves-effect waves-light btn" onClick={this.onClick}>Login</a></div>
+    }
 }
 
 export default LoginForm
